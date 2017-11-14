a111b31b13d300fb6ee99a3fda57fe016a571550
Laura M. Travin <laura.travin@gmail.com>
2017-11-14 20:10:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ function getFirstSelector(selector){
  return document.querySelector(selector)
 }
 
-function nestedTarget(value){
-  return document.querySelector(#nested ${value})
-}
+// function nestedTarget(value){
+//   return document.querySelector(#nested ${value})
+// }
 
 function nestedTarget(value){
   return document.querySelector('#nested ${value}')
 }
-
