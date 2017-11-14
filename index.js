5b16b313ce4aa473cee2dcc6173b6500444b94fb
Laura M. Travin <laura.travin@gmail.com>
2017-11-14 20:21:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ function getFirstSelector(selector){
 //   return document.querySelector('#nested .target')
 // }
 //
- //dont work
-function nestedTarget(value){
-  return document.querySelector(`#nested ${value}`)
-}
+//  //dont work
+// function nestedTarget(value){
+//   return document.querySelector(`#nested ${value}`)
+// }
 // //dont work
 // function nestedTarget(value){
 //   var class0 = '#nested';
 //   return document.querySelector('${class0} ${value}')
 // }
 //dont work
-// function nestedTarget(value){
-//   var class0 = '#nested';
-//   return document.querySelector(`${class0} ${value}`)
-// }
+function nestedTarget(value){
+  var class0 = '#nested';
+  return document.querySelector(`${class0} ${value}`)
+}
