607b1f554ee0f87a260d47c8927aa1f6c4e5cf0c
Laura M. Travin <laura.travin@gmail.com>
2017-11-14 23:10:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ function nestedTarget(){
   return document.querySelector('#nested .target')
 }
 
+function increaseRankBy(n){
+  const list = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i=0; i < list.length ; i++){
+    list[i].innerHTML = list[i].innerHTML + parseInt(n);
+  }
+}
 
 function deepestChild(){
   
