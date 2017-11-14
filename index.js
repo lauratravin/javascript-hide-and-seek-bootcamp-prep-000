c46d86debe5764eda64c19431de1a93a273993e3
Laura M. Travin <laura.travin@gmail.com>
2017-11-14 20:59:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,22 +3,7 @@ function getFirstSelector(selector){
  return document.querySelector(selector)
 }
 
-//works but wrong
-// function nestedTarget(value){
-//   return document.querySelector('#nested .target')
-// }
-//
-//  //dont work
-// function nestedTarget(value){
-//   return document.querySelector(`#nested ${value}`)
-// }
-// //dont work
-// function nestedTarget(value){
-//   var class0 = '#nested';
-//   return document.querySelector('${class0} ${value}')
-// }
-//dont work
+works but wrong
 function nestedTarget(value){
-  var class0 = '#nested';
-  return document.querySelector(`${class0} ${value}`)
+  return document.querySelector('#nested .target')
 }
