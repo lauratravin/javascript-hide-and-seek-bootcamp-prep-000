c599ae99cfc8cd15a8a585296a16159ed8f115de
Laura M. Travin <laura.travin@gmail.com>
2017-11-14 20:16:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,22 @@ function getFirstSelector(selector){
  return document.querySelector(selector)
 }
 
-// function nestedTarget(value){
-//   return document.querySelector(#nested ${value})
-// }
+//works but wrong
+function nestedTarget(value){
+  return document.querySelector('#nested .target')  
+} 
 
+ //dont work
+function nestedTarget(value){
+  return document.querySelector(#nested ${value})  
+} 
+//dont work
 function nestedTarget(value){
   var class0 = '#nested';
-  return document.querySelector('${class0} ${value}')
+  return document.querySelector('${class0} ${value}') 
+}
+//dont work
+function nestedTarget(value){
+  var class0 = '#nested';
+  return document.querySelector(`${class0} ${value}`) 
 }
