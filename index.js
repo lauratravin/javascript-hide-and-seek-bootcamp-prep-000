e28f149d9eb2761236a394e514469f33cd563a9e
Laura M. Travin <laura.travin@gmail.com>
2017-11-15 01:32:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function getFirstSelector(selector){
+ return document.querySelector(selector)
+}
+
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  const list = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i=0; i < list.length ; i++){
+    list[i].innerHTML = parseInt(list[i].innerHTML) + parseInt(n);
+  }
+}
+
+function deepestChild(){
+   const list = document.querySelectorAll('div#grand-node');
+    return list[0].children[0].children[0].children[0].children[0];
+//   return  document.querySelector('#grand-node div div div div');  //MOCHA ANSWER
+}
