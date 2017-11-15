70052c7a5ac5276bfa013381a2fe1e72dcb15b1f
Laura M. Travin <laura.travin@gmail.com>
2017-11-15 00:57:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function increaseRankBy(n){
 function deepestChild(){
    const list = document.querySelectorAll('div#grand-node');
 
-    return (list[0].children[0].children[0].children[0].innerHTML).trim();
-    //document.querySelector('#grand-node div div div div')
+  //  return (list[0].children[0].children[0].children[0].innerHTML).trim(); //complicate one.
+    document.querySelector('#grand-node div div div div')  //easy answer I guess
 }
